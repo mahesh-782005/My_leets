ecd40ad76f979cbc4231718aa78d1ccdf6cb53be
C S Mahesh Bhoopathi <maheshbhoopathi782005@gmail.com>
2026-03-25 10:51:05 +0530

    Time: 11 ms (61.61%) | Memory: 19.2 MB (99.67%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sol(self, c, t, res, start, current_sum):
+        if current_sum == t:
+            self.op.append(list(res))
+            return
+
+        for i in range(start, len(c)):
+            if i > start and c[i] == c[i-1]:
+                continue
+            
+            if current_sum + c[i] <= t:
+                res.append(c[i])
+                self.sol(c, t, res, i + 1, current_sum + c[i])
+                res.pop()
+        
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        self.op = [] 
+        candidates.sort()
+        self.sol(candidates, target, [], 0, 0)
+        return self.op
