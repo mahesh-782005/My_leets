a89c393e892edeff2fa0069000c8d268927d76cb
C S Mahesh Bhoopathi <maheshbhoopathi782005@gmail.com>
2026-06-18 10:08:52 +0530

    Time: 15 ms (61.48%) | Memory: 45.2 MB (45.77%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,47 +1,36 @@
 class Solution {
-    public boolean check(String s) {
-        if (s.length() <= 1) {
-            return true;
-        }
-        
-        int i = 0;
-        int j = s.length() - 1;
-        int flag = 1;
-        while (i < j) {
-            if (s.charAt(i) != s.charAt(j)) {
-                flag = 0;
-                break;
-            }
-            i++;
-            j--;
-        }
-        
-        if (flag == 0) {
-            return false;
-        }
-        return true;
-    }
-
     public String longestPalindrome(String s) {
-        int l = s.length();
-        if (l == 0) {
-            return "";
-        } 
-        
-        int maxlen = 1;
-        String res = s.substring(0, 1);
-        
-        for (int i = 0; i < l; i++) {
-            for (int j = i + 1; j <= l; j++) {
-                String x = s.substring(i, j);
-                if (check(x)) {
-                    int len = j - i;
-                    if (maxlen < len) {
-                        maxlen = len;
-                        res = x;
-                    }
-                }
+        int l=0, maxl =0;
+        int i =0, j =0;
+        int n = s.length();
+        String res = "";
+        for(int k =0; k<n;k++){
+            i = k;
+            j = k;
+            while(i>=0 && j<n && s.charAt(j) == s.charAt(i)){
+                i--;
+                j++;
             }
+            l = j-i+1;
+            if (l >maxl){
+                res = s.substring(i+1, j);
+                maxl = l;
+            }
+        }
+        for(int k=1; k<n; k++){
+            i = k-1;
+            j = k;
+            while(i>=0 && j<n && s.charAt(j) == s.charAt(i)){
+        
+                i--;
+                j++;
+            }
+            l = j-i+1;
+            if (l >maxl){
+                res = s.substring(i+1, j);
+                maxl = l;
+            }
+
         }
         return res;
     }
